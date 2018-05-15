d88180a7c1c79d362b416e76d11c2408b493f9fd
naoyao <na0418end.ne@gmail.com>
2018-05-15 18:08:58 +0900

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,88 @@
+
+//========================================================
+// // ドットインストール #15〜
+// ========================================================
+(function(){
+  'use strict';
+
+
+
+  var app = new Vue({
+    el:'#app'
+  });
+
+})();
+
+
+
+
+
+// ========================================================
+// // ドットインストール #1-#14までのコード
+// ========================================================
+
+// (function(){
+//   'use strict';// 厳密なエラーチェック
+//
+// // two way data binding (to UI)
+// // ビューとUIを紐づける
+//
+//   var vm = new Vue({
+//     el: '#app',
+//     data: {
+//       newItem: '',
+//       todos: []// 空の配列にしておく
+//
+//       // todos:[]// todosがなかった場合の時。空配列にしてあげる
+//     },
+//     watch:{
+//
+//       todos:{
+//         handler:function(){
+//           localStorage.setItem('todos',JSON.stringify(this.todos));
+//           // alert('Data saved!');
+//         },
+//         deep: true
+//       }
+//     },
+//
+//     mounted: function(){
+//       this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+//     },
+//
+//     methods:{
+//       addItem: function(){
+//         var item = {
+//           title: this.newItem,
+//           isDone:false
+//         };
+//         this.todos.push(item);
+//         this.newItem = '';// 入力フォームに値が残らないようにする。
+//       },
+//       deleteItem: function(index){
+//         if (confirm('are you sure?')){
+//             this.todos.splice(index, 1);// index番目から１個削除してよ。と書いてあげる。
+//         }
+//       },
+//       purge: function(index){
+//         if (!confirm('delete finished?')){
+//           return;
+//         }
+//         this.todos = this.remaining;
+//       }
+//     },
+//
+//     // computedというキーを使って、データから動的にプロパティを計算してくれる算出プロパティを使ってみる。
+//     computed:{
+//       remaining: function(){
+//
+//         return this.todos.filter(function(todo){
+//           return !todo.isDone;// まだ残っているタスクをrerutnする。それをフィルターして items にまだ終わっていないタスクだけを入れてくれる。
+//         });
+//       }
+//     }
+//
+//
+//
+//   });
+// })();// 即時関数で囲む
